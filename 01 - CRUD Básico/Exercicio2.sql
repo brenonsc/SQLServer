55c49235b4905512e58e26010b82f54e58cf850f
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-09-18 12:33:04 -0300

    Exercício 2 resolvido

--- 01 - CRUD Básico/Exercicio2.sql
+++ 01 - CRUD Básico/Exercicio2.sql
@@ -18,7 +18,12 @@ VALUES
 ('Calça', 'Calça jeans', 'Vestuário', 5, 100.00),
 ('Tênis', 'Tênis esportivo', 'Vestuário', 2, 200.00),
 ('Celular', 'iPhone 14 Pro', 'Eletrônico', 3, 6000.00),
-('Notebook', 'Notebook Dell', 'Eletrônico', 1, 5000.00);
+('Notebook', 'Notebook Dell', 'Eletrônico', 1, 5000.00),
+('Mesa', 'Mesa de escritório', 'Móvel', 2, 300.00),
+('Cadeira', 'Cadeira de escritório', 'Móvel', 2, 200.00),
+('Copo', 'Copo de vidro', 'Utensílio', 10, 10.00),
+('Prato', 'Prato de porcelana', 'Utensílio', 10, 15.00),
+('Talher', 'Talher de inox', 'Utensílio', 10, 20.00);
 
 SELECT * FROM tb_produto WHERE Preco > 500.00;
 
